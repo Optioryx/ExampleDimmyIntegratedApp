e4e83ad357d339ff7d4e869f998c8cf8898dcd42
Vic Degraeve <vicdegraeve@me.com>
2024-04-30 13:38:28 +0200

    Git ignore

---
+++ TestAppLinks/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  TestAppLinks
+//
+//  Created by Vic Degraeve on 30/04/2024.
+//
+
+import Foundation
---
+++ TestAppLinks/Base64.swift
@@ -0,0 +1,8 @@
+//
+//  Base64.swift
+//  TestAppLinks
+//
+//  Created by Vic Degraeve on 30/04/2024.
+//
+
+import Foundation
