c528800db354ec51ca70ff38380553c23fb02126
Vic Degraeve <vicdegraeve@me.com>
2024-04-30 10:17:12 +0200

    Initial Commit

---
+++ TestAppLinks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestAppLinks
+//
+//  Created by Vic Degraeve on 30/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestAppLinks/TestAppLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppLinksApp.swift
+//  TestAppLinks
+//
+//  Created by Vic Degraeve on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
